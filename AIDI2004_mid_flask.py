355f34950f2dad3de2da94b2676cca3594cbceff
Leo <kankarchun@gmail.com>
2022-06-16 08:58:58 -0400

    first commit

---
+++ AIDI2004_mid_flask.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import numpy as np
+import pickle
+import pandas as pd
+from flask import Flask, request
+import flasgger
+from flasgger import Swagger
+
+app=Flask(__name__)
+Swagger(app)
+
+pickle_in = open("log_clf.pkl","rb")
+classifier=pickle.load(pickle_in)
+
+@app.route('/')
+def hello():
+    return "Predict"
+
+@app.route('/predict_test', methods=["POST"])
+def predict_test_class():
+    """Let's predict the class for dataset
+    This is using docstrings for specifications.
+    ---
+    parameters:  
+      - name: file
+        in: formData
+        type: file
+        required: true
+    responses:
+        200:
+            description: The output values
+        
+    """
+    df_test=pd.read_csv(request.files.get("file"))
+    prediction=classifier.predict(df_test)
+    return " The Predicated Class for the TestFile is"+ str(list(prediction))
+
+
+if __name__=='__main__':
+    app.run()
+
+
+# In[ ]:
+
+
+
+
---
+++ README.md
@@ -0,0 +1,33 @@
+# Project Title
+
+Logistic Regression ML model for Breast Cancer Wisconsin (Diagnostic) Data set
+
+## Description
+
+Logistic Regression ML model for Breast Cancer Wisconsin (Diagnostic) Data set
+
+## Getting Started
+
+### Dependencies
+
+* Python 3.6
+
+### Executing program
+
+```
+python AIDI2004_mid_flask.py
+```
+* Go to http://127.0.0.1:5000/apidocs/
+
+### Pre-trained model
+
+* log_clf.pkl for logistic regression
+* Use pickle to load the pre-trained model
+```
+log_loaded_model = pickle.load(open('log_clf.pkl', 'rb'))
+```
+
+## Version History
+
+* 0.1
+* Initial Release
